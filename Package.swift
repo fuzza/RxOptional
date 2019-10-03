36825caa9c937c036e930b45c76685c1ac104bae
Alexey Faizullov <alex.fuzza@gmail.com>
2019-10-03 20:11:28 +0300

    SPM support

--- Package.swift
+++ Package.swift
@@ -9,18 +9,16 @@ let package = Package(
         .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "RxOptional", targets: ["RxOptional"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.1")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "RxOptional", dependencies: ["RxSwift"]),
-        .testTarget(name: "RxOptionalTests", dependencies: ["RxOptional"]),
-    ]
+        .target(name: "RxOptional", dependencies: ["RxSwift", "RxCocoa"], path: "Source"),
+        .testTarget(name: "RxOptionalTests", dependencies: ["RxOptional", "Quick", "Nimble"], path: "Test")
+    ],
+    swiftLanguageVersions: [.v5]
 )
